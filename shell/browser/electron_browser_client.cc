0719d182c22fd4e6a09d9f472f83cf0544746d02
Jeremy Rose <nornagon@nornagon.net>
2024-07-08 20:10:02 -0700

    5455480: [Extensions] Allow service worker requests to continue without a cert
    https://chromium-review.googlesource.com/c/chromium/src/+/5455480

--- shell/browser/electron_browser_client.cc
+++ shell/browser/electron_browser_client.cc
@@ -615,6 +615,7 @@ void ElectronBrowserClient::AllowCertificateError(
 
 base::OnceClosure ElectronBrowserClient::SelectClientCertificate(
     content::BrowserContext* browser_context,
+    int process_id,
     content::WebContents* web_contents,
     net::SSLCertRequestInfo* cert_request_info,
     net::ClientCertIdentityList client_certs,
@@ -623,7 +624,7 @@ base::OnceClosure ElectronBrowserClient::SelectClientCertificate(
     delegate->ContinueWithCertificate(nullptr, nullptr);
   } else if (delegate_) {
     delegate_->SelectClientCertificate(
-        browser_context, web_contents, cert_request_info,
+        browser_context, process_id, web_contents, cert_request_info,
         std::move(client_certs), std::move(delegate));
   }
 
